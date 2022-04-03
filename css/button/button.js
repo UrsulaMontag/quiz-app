64a06360e89aa77b0f08112d4b2948e9195b8285
Ursula Montag <montagu666@gmail.com>
2022-04-03 15:12:56 +0200

    Add button functionality and aria labelled svg

--- css/button/button.js
+++ css/button/button.js
@@ -1,28 +1,35 @@
 export default function Button(buttonElement) {
-  const answerList = document.querySelectorAll('[data-js="answer"]');
-  const buttonCardList = buttonElement.querySelectorAll(".button__big");
-
-  const buttonLogout = buttonElement.querySelector(
-    '[data-js= "button__logout"]'
-  );
-  let loggedIn = true;
-  const buttonSubmit = buttonElement.querySelector(
-    '[data-js= "button__submit"]'
-  );
-
   buttonElement.addEventListener("click", () => {
-    if (buttonCardList) {
-      answerList.forEach((answer) => {
-        answer.classList.toggle("hidden");
-      });
-      buttonCardList.forEach((button) => {
-        button.classList.toggle("hidden");
-      });
-    } else if (buttonLogout) {
-      loggedIn = !loggedIn;
-      loggedIn
-        ? (buttonLogout.textContent = "Logout")
-        : (buttonLogout.textContent = "Login");
+    if (buttonElement.getAttribute("data-js") === "buttonCard") {
+      const parentCard = buttonElement.parentElement;
+      console.log(parentCard);
+      const answerElement = parentCard.querySelector('[data-js="answer"]');
+      answerElement.classList.toggle("hidden");
+      answerElement.classList.contains("hidden")
+        ? (buttonElement.textContent = "Show Answer")
+        : (buttonElement.textContent = "Hide Answer");
+    } else if (buttonElement.getAttribute("data-js") === "buttonLogout") {
+      buttonElement.textContent === "Login"
+        ? (buttonElement.textContent = "Logout")
+        : (buttonElement.textContent = "Login");
+    } else if (buttonElement.getAttribute("data-js") === "navBookmark") {
+      console.log(buttonElement);
+      document.querySelector('[data-js = "home"]').classList.add("hidden");
+      document
+        .querySelector('[data-js = "bookmark"]')
+        .classList.remove("hidden");
+      document.querySelector('[data-js = "create"]').classList.add("hidden");
+      document.querySelector('[data-js = "profile"]').classList.add("hidden");
+    } else if (buttonElement.getAttribute("data-js") === "navCreate") {
+      document.querySelector('[data-js = "home"]').classList.add("hidden");
+      document.querySelector('[data-js = "bookmark"]').classList.add("hidden");
+      document.querySelector('[data-js = "create"]').classList.remove("hidden");
+      document.querySelector('[data-js = "profile"]').classList.add("hidden");
+    } else if (buttonElement.getAttribute("data-js") === "navHome") {
+      document.querySelector('[data-js = "home"]').classList.remove("hidden");
+      document.querySelector('[data-js = "bookmark"]').classList.add("hidden");
+      document.querySelector('[data-js = "create"]').classList.add("hidden");
+      document.querySelector('[data-js = "profile"]').classList.add("hidden");
     }
   });
 }
