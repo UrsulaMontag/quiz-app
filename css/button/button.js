2bc6e347627496026289356c24123604daa83cdb
Ursula Montag <montagu666@gmail.com>
2022-04-05 18:26:46 +0200

    Save before interact

--- css/button/button.js
+++ css/button/button.js
@@ -13,29 +13,26 @@ export default function Button(buttonElement) {
         ? (buttonElement.textContent = "Logout")
         : (buttonElement.textContent = "Login");
     } else if (buttonElement.getAttribute("data-js") === "navBookmark") {
-      document.querySelector('[data-js = "home"]').classList.add("hidden");
-      document
-        .querySelector('[data-js = "bookmark"]')
-        .classList.remove("hidden");
-      document.querySelector('[data-js = "create"]').classList.add("hidden");
-      document.querySelector('[data-js = "profile"]').classList.add("hidden");
+      document.querySelector('[data-js="home"]').classList.add("hidden");
+      document.querySelector('[data-js="bookmark"]').classList.remove("hidden");
+      document.querySelector('[data-js="form"]').classList.add("hidden");
+      document.querySelector('[data-js="profile"]').classList.add("hidden");
+      console.log("hallo");
     } else if (buttonElement.getAttribute("data-js") === "navCreate") {
-      document.querySelector('[data-js = "home"]').classList.add("hidden");
-      document.querySelector('[data-js = "bookmark"]').classList.add("hidden");
-      document.querySelector('[data-js = "create"]').classList.remove("hidden");
-      document.querySelector('[data-js = "profile"]').classList.add("hidden");
+      document.querySelector('[data-js="home"]').classList.add("hidden");
+      document.querySelector('[data-js="bookmark"]').classList.add("hidden");
+      document.querySelector('[data-js="form"]').classList.remove("hidden");
+      document.querySelector('[data-js="profile"]').classList.add("hidden");
     } else if (buttonElement.getAttribute("data-js") === "navProfile") {
-      document.querySelector('[data-js = "home"]').classList.add("hidden");
-      document.querySelector('[data-js = "bookmark"]').classList.add("hidden");
-      document.querySelector('[data-js = "create"]').classList.add("hidden");
-      document
-        .querySelector('[data-js = "profile"]')
-        .classList.remove("hidden");
+      document.querySelector('[data-js="home"]').classList.add("hidden");
+      document.querySelector('[data-js="bookmark"]').classList.add("hidden");
+      document.querySelector('[data-js="form"]').classList.add("hidden");
+      document.querySelector('[data-js="profile"]').classList.remove("hidden");
     } else if (buttonElement.getAttribute("data-js") === "navHome") {
-      document.querySelector('[data-js = "home"]').classList.remove("hidden");
-      document.querySelector('[data-js = "bookmark"]').classList.add("hidden");
-      document.querySelector('[data-js = "create"]').classList.add("hidden");
-      document.querySelector('[data-js = "profile"]').classList.add("hidden");
+      document.querySelector('[data-js="home"]').classList.remove("hidden");
+      document.querySelector('[data-js="bookmark"]').classList.add("hidden");
+      document.querySelector('[data-js="form"]').classList.add("hidden");
+      document.querySelector('[data-js="profile"]').classList.add("hidden");
     }
   });
 }
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import Button from "./css/button/button.js";
 import Bookmark from "./css/bookmark/bookmark.js";
+import CardQuestions from "./css/card/cardData.js";
 
 const Cards = document.querySelectorAll(".card");
 const Buttons = document.querySelectorAll(".button");
