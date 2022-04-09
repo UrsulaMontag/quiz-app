cc49733cb379cf5434278ce1920222b3fb1abbeb
Ursula Montag <montagu666@gmail.com>
2022-04-09 23:32:47 +0200

    And the bookmark will also work

--- css/bookmark/bookmark.js
+++ css/bookmark/bookmark.js
@@ -2,7 +2,7 @@ export default function Bookmark(cardElement) {
   const bookmark = cardElement.querySelector('[data-js="bookmark1"]');
   bookmark.addEventListener('click', () => {
     cardElement.isBookmarked = !cardElement.isBookmarked;
-    bookmark.classList.toggle(' card__bookmark--active');
+    bookmark.classList.toggle('card__bookmark--active');
     bookmark.setAttribute('aria-pressed', cardElement.isBookmarked);
   });
 }
